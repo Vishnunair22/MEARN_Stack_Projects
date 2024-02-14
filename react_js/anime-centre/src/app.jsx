dd1e35b141426e1ea6f06caa4dc471ffc0595cb0
Vishnu Nair <96977850+Vishnunair22@users.noreply.github.com>
2024-02-14 17:37:22 +0530

    anime hub project

---
+++ react_js/anime-centre/src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css';
+import './app.css';
+import NavBar from './components/navbar/NavBar';
+
+const app = () => {
+  return (
+    <header>
+      <NavBar/>
+    </header>
+  )
+}
+
+export default app
