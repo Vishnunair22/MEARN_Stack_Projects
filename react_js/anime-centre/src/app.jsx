7cd78aed10027d10ac03d5543d65051114be7aef
Vishnu Nair <96977850+Vishnunair22@users.noreply.github.com>
2024-02-14 19:47:20 +0530

    added main section trending and popular sections.

--- react_js/anime-centre/src/app.jsx
+++ react_js/anime-centre/src/app.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import './index.css';
 import './app.css';
 import NavBar from './components/navbar/NavBar';
+import MainPart from './components/main/MainPart';
 
 const app = () => {
   return (
+    <>
     <header>
       <NavBar/>
     </header>
+    <main>
+      <MainPart/>
+    </main>
+    </>
   )
 }
 
