9dc8b0a70ea36bd16acc9e65bf2a6a5f74ac148d
Vishnu Nair <96977850+Vishnunair22@users.noreply.github.com>
2024-02-20 19:56:38 +0530

    Updated code

--- react_js/calculator/src/App.jsx
+++ react_js/calculator/src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleButtonClick = (value) => {
+    document.getElementById('display').value += value;
+  };
+
+  const handleClear = () => {
+    document.getElementById('display').value = '';
+  };
+
+  const calculateResult = () => {
+    const result = eval(document.getElementById('display').value);
+    document.getElementById('display').value = result;
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h1>
+        <i className="fa-solid fa-calculator"></i> Calculator
+      </h1>
+      <div className="calculator">
+        <input type="text" name="display" id="display" />
+        <div>
+          <button onClick={() => handleButtonClick('(')}>(</button>
+          <button onClick={() => handleButtonClick(')')}>)</button>
+          <button onClick={() => handleClear()}>DE</button>
+          <button onClick={() => handleButtonClick('/')} aria-label="divide">
+            <i className="fa-solid fa-divide"></i>
+          </button>
+        </div>
+        <div>
+          <button onClick={() => handleButtonClick('7')}>7</button>
+          <button onClick={() => handleButtonClick('8')}>8</button>
+          <button onClick={() => handleButtonClick('9')}>9</button>
+          <button onClick={() => handleButtonClick('*')} aria-label="multiply">
+            <i className="fa-solid fa-xmark"></i>
+          </button>
+        </div>
+        <div>
+          <button onClick={() => handleButtonClick('4')}>4</button>
+          <button onClick={() => handleButtonClick('5')}>5</button>
+          <button onClick={() => handleButtonClick('6')}>6</button>
+          <button onClick={() => handleButtonClick('-')} aria-label="minus">
+            <i className="fa-solid fa-minus"></i>
+          </button>
+        </div>
+        <div>
+          <button onClick={() => handleButtonClick('1')}>1</button>
+          <button onClick={() => handleButtonClick('2')}>2</button>
+          <button onClick={() => handleButtonClick('3')}>3</button>
+          <button onClick={() => handleButtonClick('+')} aria-label="plus">
+            <i className="fa-solid fa-plus"></i>
+          </button>
+        </div>
+        <div>
+          <button onClick={() => handleClear()}>AC</button>
+          <button onClick={() => handleButtonClick('0')}>0</button>
+          <button onClick={() => handleButtonClick('.')}>.</button>
+          <button onClick={() => calculateResult()} aria-label="result">
+            <i className="fa-solid fa-equals"></i>
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
