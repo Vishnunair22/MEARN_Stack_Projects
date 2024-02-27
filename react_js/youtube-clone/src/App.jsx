0e12d0d6a3abed0e8cf61bf1fa9bb57d8770846b
Vishnu Nair <96977850+Vishnunair22@users.noreply.github.com>
2024-02-27 10:44:13 +0530

    Sidebar added

--- react_js/youtube-clone/src/App.jsx
+++ react_js/youtube-clone/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './App.css'
 import NavBar from './components/navbar/NavBar'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Video from './pages/video/Video'
 
 function App() {
   return (
     <>
       <NavBar/>
+      <Router>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/video/:categoryId/:videoId' element={<Video/>} />
+      </Routes>
+      </Router>
       </>
   )
 }
---
+++ react_js/youtube-clone/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './home.css'
+import SideBar from '../../components/sidebar/SideBar'
+
+function Home() {
+  return (
+    <>
+      <SideBar/>
+    </>
+  )
+}
+
+export default Home
