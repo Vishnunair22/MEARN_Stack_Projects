9ad640ab9489036b7e7c9feb06e724741be8d651
Vishnu Nair <96977850+Vishnunair22@users.noreply.github.com>
2024-03-01 13:15:44 +0530

    Design complete

---
+++ react_js/youtube-clone/src/Components/Recommended/Recommended.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import './Recommended.css'
+import thumbnail1 from '../../assets/thumbnail1.png'
+import thumbnail2 from '../../assets/thumbnail2.png'
+import thumbnail3 from '../../assets/thumbnail3.png'
+import thumbnail4 from '../../assets/thumbnail4.png'
+import thumbnail5 from '../../assets/thumbnail5.png'
+import thumbnail6 from '../../assets/thumbnail6.png'
+import thumbnail7 from '../../assets/thumbnail7.png'
+import thumbnail8 from '../../assets/thumbnail8.png'
+
+
+const Recommended = () => {
+  return (
+    <div className='recommended'>
+        <div className="side-video-list">
+            <img src={thumbnail1} alt="video thumbnail" />
+            <div className="vid-info">
+                <h3>Popular Places in Japan</h3>
+                <p>Nick Vlogs</p>
+                <p>2.9M views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={thumbnail1} alt="video thumbnail" />
+            <div className="vid-info">
+                <h3>Popular Places in Japan</h3>
+                <p>Nick Vlogs</p>
+                <p>2.9M views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={thumbnail1} alt="video thumbnail" />
+            <div className="vid-info">
+                <h3>Popular Places in Japan</h3>
+                <p>Nick Vlogs</p>
+                <p>2.9M views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={thumbnail1} alt="video thumbnail" />
+            <div className="vid-info">
+                <h3>Popular Places in Japan</h3>
+                <p>Nick Vlogs</p>
+                <p>2.9M views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={thumbnail1} alt="video thumbnail" />
+            <div className="vid-info">
+                <h3>Popular Places in Japan</h3>
+                <p>Nick Vlogs</p>
+                <p>2.9M views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={thumbnail1} alt="video thumbnail" />
+            <div className="vid-info">
+                <h3>Popular Places in Japan</h3>
+                <p>Nick Vlogs</p>
+                <p>2.9M views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={thumbnail1} alt="video thumbnail" />
+            <div className="vid-info">
+                <h3>Popular Places in Japan</h3>
+                <p>Nick Vlogs</p>
+                <p>2.9M views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={thumbnail1} alt="video thumbnail" />
+            <div className="vid-info">
+                <h3>Popular Places in Japan</h3>
+                <p>Nick Vlogs</p>
+                <p>2.9M views</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Recommended
