ff5d196565c9bd43ee0044dfae111e951d68faa3
Vishnu Nair <96977850+Vishnunair22@users.noreply.github.com>
2024-03-05 17:33:38 +0530

    Play Video Component completed

--- react_js/youtube-clone/src/Components/PlayVideo/PlayVideo.jsx
+++ react_js/youtube-clone/src/Components/PlayVideo/PlayVideo.jsx
@@ -1,104 +1,112 @@
-import React from 'react'
-import './PlayVideo.css'
-import video1 from '../../assets/video.mp4'
-import like from '../../assets/like.png'
-import dislike from '../../assets/dislike.png'
-import share from '../../assets/share.png'
-import save from '../../assets/save.png'
-import user_profile from '../../assets/user_profile.jpg'
-import megan from '../../assets/megan.png'
+import React, { useEffect, useState } from 'react';
+import './PlayVideo.css';
+import video1 from '../../assets/video.mp4';
+import like from '../../assets/like.png';
+import dislike from '../../assets/dislike.png';
+import share from '../../assets/share.png';
+import save from '../../assets/save.png';
+import user_profile from '../../assets/user_profile.jpg';
+import megan from '../../assets/megan.png';
+import { API_KEY, value_converter } from '../../data';
+import moment from 'moment';
 
-const PlayVideo = () => {
-  return (
-    <div className='play-video'>
-        <video src={video1} controls autoPlay muted></video>
-        <h3>Top Places to Visit in UK</h3>
-        <div className="play-video-info">
-            <p>1.4M views &bull; 3 weeks ago</p>
-            <div>
-            <span><img src={like} alt="like button" />200k</span>
-            <span><img src={dislike} alt="dislike button" />2.1k</span>
-            <span><img src={share} alt="share button" />Share</span>            
-            <span><img src={save} alt="save button" />Save</span>
-        </div>
-        </div>   
+const PlayVideo = ({ videoId }) => {
+    const [apiData, setApiData] = useState(null);
+    const [channelData, setChannelData] = useState(null);
+    const [commentData, setCommentData] = useState([]); 
 
-        <div className="publisher">
-            <img src={megan} alt="megan" />
-            <div>
-                <p>5 Minutes Crafts</p>
-                <span>1.78M Subscribers</span>
-            </div>
-            <button>Subscribe</button>
-        </div>
-        <div className="vid-discription">
-            <p>This Channel focus on Arts & Crafts.Enter the world of color and beauty.</p>
-            <p>Follow us for more such videos</p>
-            <hr />
-            <h4>1.8k Comments</h4>
-            <div className="comment">
-                <img src={user_profile} alt="user profile imege" />
-                <div>
-                    <h3>Steve Willson <span>2 days ago</span></h3>
-                    <p>Really interesting video.Will check more of your videos.</p>
-                    <div className="comment-actions">
-                        <img src={like} alt="like button" />
-                        <span>2.1k</span>
-                        <img src={dislike} alt="dislike button" />
-                    </div>
-                </div>
-            </div>
-            <div className="comment">
-                <img src={user_profile} alt="user profile imege" />
-                <div>
-                    <h3>Steve Willson <span>2 days ago</span></h3>
-                    <p>Really interesting video.Will check more of your videos.</p>
-                    <div className="comment-actions">
-                        <img src={like} alt="like button" />
-                        <span>2.1k</span>
-                        <img src={dislike} alt="dislike button" />
-                    </div>
-                </div>
-            </div>
-            <div className="comment">
-                <img src={user_profile} alt="user profile imege" />
-                <div>
-                    <h3>Steve Willson <span>2 days ago</span></h3>
-                    <p>Really interesting video.Will check more of your videos.</p>
-                    <div className="comment-actions">
-                        <img src={like} alt="like button" />
-                        <span>2.1k</span>
-                        <img src={dislike} alt="dislike button" />
-                    </div>
-                </div>
-            </div>
-            <div className="comment">
-                <img src={user_profile} alt="user profile imege" />
-                <div>
-                    <h3>Steve Willson <span>2 days ago</span></h3>
-                    <p>Really interesting video.Will check more of your videos.</p>
-                    <div className="comment-actions">
-                        <img src={like} alt="like button" />
-                        <span>2.1k</span>
-                        <img src={dislike} alt="dislike button" />
-                    </div>
-                </div>
-            </div>
-            <div className="comment">
-                <img src={user_profile} alt="user profile imege" />
-                <div>
-                    <h3>Steve Willson <span>2 days ago</span></h3>
-                    <p>Really interesting video.Will check more of your videos.</p>
-                    <div className="comment-actions">
-                        <img src={like} alt="like button" />
-                        <span>2.1k</span>
-                        <img src={dislike} alt="dislike button" />
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-  )
-}
+    const fetchVideoData = async () => {
+        const videoDetails_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`;
+        try {
+            const response = await fetch(videoDetails_url);
+            const data = await response.json();
+            setApiData(data.items[0]);
+        } catch (error) {
+            console.error('Error fetching video data:', error);
+        }
+    };
 
-export default PlayVideo
+    const fetchOtherData = async () => {
+        if (apiData && apiData.snippet && apiData.snippet.channelId) {
+            const channelData_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`;
+            try {
+                const response = await fetch(channelData_url);
+                const data = await response.json();
+                setChannelData(data.items[0]);
+            } catch (error) {
+                console.error('Error fetching channel data:', error);
+            }
+        }
+    };
+
+    const fetchCommentData = async () => {
+        const commentData_url = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=50&videoId=${videoId}&key=${API_KEY}`;
+        try {
+            const response = await fetch(commentData_url);
+            const data = await response.json();
+            setCommentData(data.items);
+        } catch (error) {
+            console.error('Error fetching comment data:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchVideoData();
+    }, [videoId]);
+
+    useEffect(() => {
+        fetchOtherData();
+    }, [apiData]);
+
+    useEffect(() => {
+        if (apiData) {
+            fetchCommentData();
+        }
+    }, [apiData]);
+
+    return (
+        <div className='play-video'>
+            <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} title="video" allowFullScreen></iframe>
+            <h3>{apiData ? apiData.snippet.title : "Title here"}</h3>
+            <div className="play-video-info">
+                <p>{apiData ? value_converter(apiData.statistics.viewCount) : "No Views"} &bull; {apiData ? moment(apiData.snippet.publishedAt).fromNow() : "Published date"}</p>
+                <div>
+                    <span><img src={like} alt="like button" />{apiData ? value_converter(apiData.statistics.likeCount) : "212"}</span>
+                    <span><img src={dislike} alt="dislike button" /></span>
+                    <span><img src={share} alt="share button" />Share</span>
+                    <span><img src={save} alt="save button" />Save</span>
+                </div>
+            </div>
+
+            <div className="publisher">
+                <img src={channelData ? channelData.snippet.thumbnails.default.url : megan} alt="publisher" />
+                <div>
+                    <p>{apiData ? apiData.snippet.channelTitle : ""}</p>
+                    <span>{channelData ? value_converter(channelData.statistics.subscriberCount) : "1.78M"} Subscribers</span>
+                </div>
+                <button>Subscribe</button>
+            </div>
+            <div className="vid-description">
+                <p>{apiData ? apiData.snippet.description.slice(0, 250) : "There is no available description for this video"}</p>
+                <hr />
+                <h4>{apiData ? value_converter(apiData.statistics.commentCount) : "0"} Comments</h4>
+                {commentData.map((item, index) => (
+                    <div className="comment" key={index}>
+                        <img src={item.snippet.topLevelComment.snippet.authorProfileImageUrl} alt="user profile image" />
+                        <div>
+                            <h3>{item.snippet.topLevelComment.snippet.authorDisplayName} <span>{moment(item.snippet.topLevelComment.snippet.publishedAt).fromNow()}</span></h3>
+                            <p>{item.snippet.topLevelComment.snippet.textOriginal}</p>
+                            <div className="comment-actions">
+                                <img src={like} alt="like button" />
+                                <span>Like</span>
+                                <img src={dislike} alt="dislike button" />
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default PlayVideo;
--- react_js/youtube-clone/src/Components/Recommended/Recommended.jsx
+++ react_js/youtube-clone/src/Components/Recommended/Recommended.jsx
@@ -1,84 +1,48 @@
-import React from 'react'
-import './Recommended.css'
-import thumbnail1 from '../../assets/thumbnail1.png'
-import thumbnail2 from '../../assets/thumbnail2.png'
-import thumbnail3 from '../../assets/thumbnail3.png'
-import thumbnail4 from '../../assets/thumbnail4.png'
-import thumbnail5 from '../../assets/thumbnail5.png'
-import thumbnail6 from '../../assets/thumbnail6.png'
-import thumbnail7 from '../../assets/thumbnail7.png'
-import thumbnail8 from '../../assets/thumbnail8.png'
+import React, { useEffect, useState } from 'react';
+import './Recommended.css';
+import { API_KEY, value_converter } from '../../data';
+import thumbnail1 from '../../assets/thumbnail1.png';
+import thumbnail2 from '../../assets/thumbnail2.png';
+import thumbnail3 from '../../assets/thumbnail3.png';
+import thumbnail4 from '../../assets/thumbnail4.png';
+import thumbnail5 from '../../assets/thumbnail5.png';
+import thumbnail6 from '../../assets/thumbnail6.png';
+import thumbnail7 from '../../assets/thumbnail7.png';
+import thumbnail8 from '../../assets/thumbnail8.png';
 
+const Recommended = ({ categoryId }) => {
+    const [apiData, setApiData] = useState([]);
+    
+    const fetchApiData = async () => {
+        const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&maxResults=45&videoCategoryId=${categoryId}&key=${API_KEY}`;
+        
+        try {
+            const response = await fetch(relatedVideo_url);
+            const data = await response.json();
+            setApiData(data.items);
+        } catch (error) {
+            console.error('Error fetching API data:', error);
+        }
+    };
+    
+    useEffect(() => {
+        fetchApiData();
+    }, [categoryId]);
+    
+    return (
+        <div className='recommended'>
+            {apiData.map((item, index) => (
+                <div className="side-video-list" key={index}>
+                    <img src={item.snippet.thumbnails.medium.url} alt="video thumbnail" />
+                    <div className="vid-info">
+                        <h3>{item.snippet.title}</h3>
+                        <p>{item.snippet.channelTitle}</p>
+                        <p>{value_converter(item.statistics.viewCount)} views</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
 
-const Recommended = () => {
-  return (
-    <div className='recommended'>
-        <div className="side-video-list">
-            <img src={thumbnail1} alt="video thumbnail" />
-            <div className="vid-info">
-                <h3>Popular Places in Japan</h3>
-                <p>Nick Vlogs</p>
-                <p>2.9M views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail1} alt="video thumbnail" />
-            <div className="vid-info">
-                <h3>Popular Places in Japan</h3>
-                <p>Nick Vlogs</p>
-                <p>2.9M views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail1} alt="video thumbnail" />
-            <div className="vid-info">
-                <h3>Popular Places in Japan</h3>
-                <p>Nick Vlogs</p>
-                <p>2.9M views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail1} alt="video thumbnail" />
-            <div className="vid-info">
-                <h3>Popular Places in Japan</h3>
-                <p>Nick Vlogs</p>
-                <p>2.9M views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail1} alt="video thumbnail" />
-            <div className="vid-info">
-                <h3>Popular Places in Japan</h3>
-                <p>Nick Vlogs</p>
-                <p>2.9M views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail1} alt="video thumbnail" />
-            <div className="vid-info">
-                <h3>Popular Places in Japan</h3>
-                <p>Nick Vlogs</p>
-                <p>2.9M views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail1} alt="video thumbnail" />
-            <div className="vid-info">
-                <h3>Popular Places in Japan</h3>
-                <p>Nick Vlogs</p>
-                <p>2.9M views</p>
-            </div>
-        </div>
-        <div className="side-video-list">
-            <img src={thumbnail1} alt="video thumbnail" />
-            <div className="vid-info">
-                <h3>Popular Places in Japan</h3>
-                <p>Nick Vlogs</p>
-                <p>2.9M views</p>
-            </div>
-        </div>
-    </div>
-  )
-}
-
-export default Recommended
+export default Recommended;
