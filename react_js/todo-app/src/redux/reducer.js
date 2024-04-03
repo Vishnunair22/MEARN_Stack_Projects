294dfa96a73a9f94c9f8b81558b61197120689fa
Vishnu Nair <96977850+Vishnunair22@users.noreply.github.com>
2024-04-03 16:52:09 +0530

    Update reducer.js

--- react_js/todo-app/src/redux/reducer.js
+++ react_js/todo-app/src/redux/reducer.js
@@ -6,8 +6,6 @@ const addTodoReducer = createSlice({
   name: "todos",
   initialState,
   reducers: {
-    //here we will write our reducer
-    //Adding todos
     addTodos: (state, action) => {
       state.push(action.payload);
       return state;
@@ -16,7 +14,6 @@ const addTodoReducer = createSlice({
     removeTodos: (state, action) => {
       return state.filter((item) => item.id !== action.payload);
     },
-    //update todos
     updateTodos: (state, action) => {
       return state.map((todo) => {
         if (todo.id === action.payload.id) {
@@ -28,7 +25,6 @@ const addTodoReducer = createSlice({
         return todo;
       });
     },
-    //completed
     completeTodos: (state, action) => {
       return state.map((todo) => {
         if (todo.id === action.payload) {
