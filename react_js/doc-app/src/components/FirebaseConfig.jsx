78ab36b0e4277853ff7d430237b9c8db3e537a69
Vishnu Nair <96977850+Vishnunair22@users.noreply.github.com>
2024-04-29 08:35:56 +0530

    Main Project

--- react_js/doc-app/src/components/FirebaseConfig.jsx
+++ react_js/doc-app/src/components/FirebaseConfig.jsx
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDu2dMpPZabmLxDFHaixTJye6MO8SL6A2Y",
-  authDomain: "doc-app-eb919.firebaseapp.com",
-  projectId: "doc-app-eb919",
-  storageBucket: "doc-app-eb919.appspot.com",
-  messagingSenderId: "513757712859",
-  appId: "1:513757712859:web:427cc991f941b0e8d497fe",
-  measurementId: "G-WYFP0D91S2"
+  apiKey: "AIzaSyAUloOPJDtpcl_lnY6LqG39WZKjeKoXEPo",
+  authDomain: "doc-app-ec626.firebaseapp.com",
+  projectId: "doc-app-ec626",
+  storageBucket: "doc-app-ec626.appspot.com",
+  messagingSenderId: "572108823020",
+  appId: "1:572108823020:web:4996dfe69ad9cd6ee048c9",
+  measurementId: "G-JVRXH6CK97"
 };
 
 export const app = initializeApp(firebaseConfig);
